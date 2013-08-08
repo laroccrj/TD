ff75463d44fd8950d062651dea02cfc454d72f10
Robert La Rocca <laroccrj@alfredstate.edu>
2013-08-08 00:25:35 -0400

    Made Unit class less specific

--- TD/Assets/Scripts/AI/Unit.cs
+++ TD/Assets/Scripts/AI/Unit.cs
@@ -9,41 +9,57 @@ public class Unit : MonoBehaviour {
 	public float attackRange;
 	public float aggroRange;
 	public float attackSpeed;
-	public bool alive = true;
+	public float moveSpeed;
 	
+	private Unit target;
 	private int currentHealth;
 	private float nextAttack = 0;
-	private Vector3 startPosition;
 	
 	public void Start() {
-		this.startPosition = transform.position;
 		this.currentHealth = this.health;
+		this.OnStart();
 	}
 	
-	public void Damage(int damage) {
+	public virtual void OnStart() {}
+	
+	public void Update() {
+		
+		this.OnUpdate();
+		
+		if(this.target == null) return;
+		
+		if(Vector3.Distance(transform.position, target.transform.position) < attackRange) {
+			if(nextAttack < Time.time) {
+				Attack(this.target);
+			}
+		} else {
+				
+		}
+	}
+	
+	public virtual void OnUpdate() {}
+	
+	public void Damage(Unit attacker, int damage) {
 		this.currentHealth -= damage;
+		this.OnDamage(attacker, damage);
 		if(this.currentHealth <= 0) {
 			this.Death();
 		}
 	}
 	
+	public virtual void OnDamage(Unit attacker, int damage) {}
+	
 	public void Attack(Unit target) {
-		target.Damage(this.damage);
+		target.Damage(this, this.damage);
 		this.nextAttack = Time.time + this.attackSpeed;
+		this.OnAttack(target);
 	}
+	
+	public virtual void OnAttack(Unit target) {}
 	
 	public int getCurrentHealth() {
 		return this.currentHealth;	
 	}
 	
-	private void Death() {
-		alive = false;
-		renderer.enabled = false;
-	}
-	
-	public void Respawn() {
-		alive = true;
-		renderer.enabled = true;
-		transform.position = startPosition;
-	}
+	public virtual void Death() {}
 }
